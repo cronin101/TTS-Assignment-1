3e961e3a3647dc5137650bf76735c99ca358e076
Aaron Cronin <cronin@include.cat>
2013-10-05 00:23:58 +0100

    QueryScore implemented

--- tfidf.py
+++ tfidf.py
@@ -1,17 +1,17 @@
 from tokenize import *
 import os
+from math import log
 from itertools import chain
 
 class TFIDFScorer:
   def __init__(self, filename, queries_file, documents_file, k):
-    self.filename = filename
-    self.queries = FileTokenizer(queries_file)
-    self.documents = FileTokenizer(documents_file)
-    self.k = k
+    self.filename, self.k = filename, k
+    self.queries, self.documents  = FileTokenizer(queries_file), FileTokenizer(documents_file)
     corpus = chain(self.queries.all(), self.documents.all())
     self.unique_words = set(chain.from_iterable(map(lambda l: l.tokens, corpus)))
     self.word_id = dict(map(lambda t: reversed(t), enumerate(self.unique_words, 1)))
     self.document_by_id = dict(map(lambda d: (d.sample_number, d), self.documents.all()))
+    self.C = len(self.documents.all())
 
   def crunch_numbers(self):
     self.compute_k_d_over_avg_d()
@@ -27,15 +27,36 @@ class TFIDFScorer:
 
   def get_tf(self, word_id, document_id):
     '''Occurrences of word i in document j'''
-    self.term_frequency.get((word_id, document_id), 0)
+    return self.term_frequency.get((word_id, document_id), 0)
+
+  def get_df(self, word_id):
+    '''Occurrences of word i in any document'''
+    return self.document_frequency.get(word_id, 0)
+
+  def tf(self, word_id, document_id):
+    tf = self.get_tf(word_id, document_id)
+    return tf / (tf + self.kd_o_avd[document_id])
+
+  def idf(self, document_id):
+    return log((self.C / self.get_df(document_id)),10)
+
+  def query_score(self, query, document_id):
+    q_word_ids = map(lambda w: self.word_id[w], query.tokens)
+    j = document_id
+    return sum(map(lambda i: q_word_ids.count(i) * self.tf(i,j) * self.idf(j), q_word_ids))
 
   def compute_term_frequency(self):
     '''For each document, record the number of times that each word appears'''
     self.term_frequency = {}
+    self.document_frequency = {}
     for document in self.documents.all():
+      doc_id = document.sample_number
       for word in document.tokens:
-        existing = self.term_frequency.get((self.word_id[word], document.sample_number), 0)
-        self.term_frequency[(self.word_id[word], document.sample_number)] = existing + 1
+        word_id = self.word_id[word]
+        existing_tf = self.get_tf(word_id, doc_id)
+        self.term_frequency[(word_id, doc_id)] = existing_tf + 1
+        existing_df = self.get_df(word_id)
+        self.document_frequency[word_id] = existing_df + 1
 
 if __name__ == "__main__":
   TFIDFScorer('derp', './qrys.txt', './docs.txt', 2.0).crunch_numbers()
