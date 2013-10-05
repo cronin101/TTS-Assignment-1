1540221671e230e04fda8e183e6df32c0c5ae215
Aaron Cronin <cronin@include.cat>
2013-10-05 13:03:13 +0100

    Finished testing overlap scorer

--- overlap_test.py
+++ overlap_test.py
@@ -6,6 +6,7 @@ import sys
 
 class TestOverlapScorer(unittest.TestCase):
   def test_no_overlaps(self):
+    '''Queries share no words with documents so the scores should be 0'''
     query_lines = [
       '1 How to roll up pants?',
       '2 How r babby formed?',
@@ -21,6 +22,18 @@ class TestOverlapScorer(unittest.TestCase):
     for query in queries:
       for document in documents:
         self.assertEqual(scorer.query_score(query, document), 0)
+
+  def test_with_overlaps(self):
+    '''Should be scored correctly regardless of puctuation'''
+    scorer = OverlapScorer('./output', [], [])
+    self.assertEqual(scorer.query_score(
+      StringTokenizer('66 How to clean combine-harvester?'),
+      StringTokenizer("88 I've got a brand new combine harvester")
+    ), 2)
+    self.assertEqual(scorer.query_score(
+      StringTokenizer('4 I ran MapReduce in the cloud and now I have too much data'),
+      StringTokenizer('9 Not a cloud in sight as Usain Bolt ran the 100m sprint')
+    ), 4)
 
 if __name__ == '__main__':
       unittest.main()
